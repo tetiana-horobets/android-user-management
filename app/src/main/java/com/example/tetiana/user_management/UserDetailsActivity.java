778cd47735f3fb3a751a53e29e6c3ea5a50e87a0
Tetiana Horobets <tanusha.sts@gmail.com>
2017-09-17 21:05:14 +0200

    show user details

--- app/src/main/java/com/example/tetiana/user_management/UserDetailsActivity.java
+++ app/src/main/java/com/example/tetiana/user_management/UserDetailsActivity.java
@@ -1,11 +1,13 @@
 package com.example.tetiana.user_management;
 
-
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
 
 import java.io.InputStream;
 
@@ -20,6 +22,8 @@ public class UserDetailsActivity extends AppCompatActivity {
         TextView age = (TextView) findViewById(R.id.user_detailed_age);
         TextView isFemale = (TextView) findViewById(R.id.user_detailed_is_female);
         LinearLayout layoutHobby = (LinearLayout) findViewById(R.id.user_detailed_hobbies);
+        ImageView image = (ImageView) findViewById(R.id.user_detailed_image);
+        ImageView back = (ImageView) findViewById(R.id.user_detailed_back);
 
         try {
             InputStream inputStream = getAssets().open("user-details.json");
@@ -39,6 +43,9 @@ public class UserDetailsActivity extends AppCompatActivity {
                 textView.setText(hobby);
                 layoutHobby.addView(textView);
             }
+            Picasso.with(this).load(user.getImage()).into(image);
+            Picasso.with(this).load(user.getBack()).into(back);
+
         } catch (Exception e) {
             e.printStackTrace();
         }
