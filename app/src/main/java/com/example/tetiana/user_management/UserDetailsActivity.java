33394e958336573caf49ebb56dbc9566c9fc839a
Tetiana Horobets <tanusha.sts@gmail.com>
2017-09-17 21:48:13 +0200

    send email button

--- app/src/main/java/com/example/tetiana/user_management/UserDetailsActivity.java
+++ app/src/main/java/com/example/tetiana/user_management/UserDetailsActivity.java
@@ -1,8 +1,11 @@
 package com.example.tetiana.user_management;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
@@ -16,7 +19,9 @@ public class UserDetailsActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.user_details_activity);
+
         TextView name = (TextView) findViewById(R.id.user_detailed_name);
         TextView email = (TextView) findViewById(R.id.user_detailed_email);
         TextView age = (TextView) findViewById(R.id.user_detailed_age);
@@ -24,30 +29,41 @@ public class UserDetailsActivity extends AppCompatActivity {
         LinearLayout layoutHobby = (LinearLayout) findViewById(R.id.user_detailed_hobbies);
         ImageView image = (ImageView) findViewById(R.id.user_detailed_image);
         ImageView back = (ImageView) findViewById(R.id.user_detailed_back);
+        Button buttonSend = (Button) findViewById(R.id.buttonSend);
 
         try {
             InputStream inputStream = getAssets().open("user-details.json");
             UserRepository userRepository = new UserRepository(inputStream);
             int userId = getIntent().getIntExtra("userId", -1);
-            User user =  userRepository.findUsersById(userId);
+            final User user =  userRepository.findUsersById(userId);
             name.setText(user.getName());
             email.setText(user.getEmail());
             age.setText(String.valueOf(user.getAge()));
-            if (user.isFemale()){
-                isFemale.setText("Female");
-            } else {
-                isFemale.setText("Male");
-            }
+            isFemale.setText(user.isFemale() ? "Female" : "Male");
+
             for(String hobby: user.getHobbies()){
                 final TextView textView = new TextView(this);
                 textView.setText(hobby);
                 layoutHobby.addView(textView);
             }
+
             Picasso.with(this).load(user.getImage()).into(image);
             Picasso.with(this).load(user.getBack()).into(back);
 
+            buttonSend.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent sendEmail = new Intent(Intent.ACTION_SEND);
+
+                    sendEmail.putExtra(Intent.EXTRA_EMAIL, user.getEmail());
+                    sendEmail.setType("message/rfc822");
+
+                    startActivity(Intent.createChooser(sendEmail, "Choose an Email client"));
+                }
+            });
         } catch (Exception e) {
             e.printStackTrace();
         }
+
     }
 }
